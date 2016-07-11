49504fa0df143eca263c9e21d5838e0db4d4c386
puling <oaketree@163.com>
2016-07-11 10:58:19 +0800

    711

---
+++ Cibb.Core.Cache/CacheService.cs
@@ -0,0 +1,40 @@
+﻿using System.Configuration;
+
+namespace Cibb.Core.Cache
+{
+    public class CacheService
+    {
+        public static string GetConn(string name)
+        {
+            var key = name;
+            var conn = CacheHelper.Get(key);
+            if (conn != null)
+                return conn.ToString();
+            else
+            {
+                conn = ConfigurationManager.ConnectionStrings[name].ConnectionString;
+                CacheHelper.Set(key, conn);
+                return conn.ToString();
+            }
+        }
+        public static T Get<T>()
+        {
+            var fileName = GetConfigFileName<T>();
+            var content = CacheHelper.Get(fileName);
+            return (T)content;
+        }
+
+        public static void Save<T>(T value)
+        {
+            var fileName = GetConfigFileName<T>();
+            CacheHelper.Set(fileName, value);
+        }
+
+        public static string GetConfigFileName<T>()
+        {
+            var fileName = typeof(T).Name;
+            return fileName;
+        }
+
+    }
+}
